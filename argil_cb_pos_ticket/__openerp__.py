d71c57dc3782d124a2254bbc3569433c6dd06aaa
German <german_442@hotmail.com>
2015-08-27 12:55:27 -0500

    Addons CB

---
+++ argil_cb_pos_ticket/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Copyright (c) 2015 Argil Consulting - http://www.argil.mx
+#    info Argil Consulting (info@argil.mx)
+############################################################################
+#    Coded by: Israel Cruz (israel.cruz@argil.mx)
+############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "POS Ticket - Casa Baltasar",
+    "version": "1.0", 
+    "author": "Argil", 
+    "category": "POS", 
+    "description": """
+POS Ticket - Casa Baltasar
+==========================
+
+Este módulo tiene la configuración del ticket del POS de Casa Baltazar
+
+    """, 
+    "website": "http://www.argil.mx", 
+    "depends" : ["point_of_sale"],
+    "data": ["pos_ticket_template.xml"],
+    "qweb" : [
+        'static/src/xml/pos_cb.xml',
+    ], 
+    "installable": True, 
+    "active": False
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
